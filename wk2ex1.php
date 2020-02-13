856ab89572d97ff4f8f2bc89a796b67d3663e894
joniilosele <21811337+joniilosele@users.noreply.github.com>
2020-02-13 17:56:12 +0000

    w2e1-e9

--- wk2ex1.php
+++ wk2ex1.php
@@ -1,11 +1,12 @@
-<? 
+<?php
 $hourlyrate	= 5.75;
 	$hoursperweek = 40;
 	$gross = $hourlyrate * $hoursperweek;
 ?>	
 <html>
-<head></head>
+<head>
 <body>
-	<p> My gross wage is <?php print(“$gross”); ?>
+	<p> My gross wage is <?php print("$gross"); ?>
 </body>
+</head>
 </html>
---
+++ wk2ex8.php
@@ -0,0 +1,14 @@
+<?php
+  $topModules[0] = "Internet Systems Development";
+  $topModules[1] = "Programming 1";
+  $topModules[2] = "Programming 2";
+  $topModules[3] = "OOAD";
+  $topModules[4] = "Programming Engineering";
+  $topModules[5] = "Web Development";
+  $topModules[6] = "Programming 6";
+
+  for($count = 0; $count < 7; $count++)
+  {
+    echo "$count module is $topModules[$count] <br/>";
+  }	
+?>
