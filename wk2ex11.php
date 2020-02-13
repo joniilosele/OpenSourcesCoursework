171c7d9ca7ba481b8fcee8e9e7a1fae202457d91
joniilosele <21811337+joniilosele@users.noreply.github.com>
2020-02-13 18:07:26 +0000

    w2e1-11

--- wk2ex11.php
+++ wk2ex11.php
@@ -10,3 +10,15 @@
     echo "Index is $index  and value is $value <br/>";
   }
 ?>
+
+<?php
+  $mymarks["year 1"] = 55;
+  $mymarks["year 2"] = 65;
+  $mymarks["year 3"] = 75;
+
+  while(list($index,$value) = each($mymarks))
+  {
+    echo "For $index my grade was  $value .<br/>";
+  }
+  echo "<br/> My best year was Year 3 when I averaged ". $mymarks["year 3"];
+?>
